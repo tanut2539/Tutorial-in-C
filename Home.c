d27fe48271f0be002a3d7ef5c25c70bb08569f1b
Tanut Leelaparsert <tanut.leel@bumail.net>
2017-01-15 01:23:44 +0700

    tutorial created home in language c

---
+++ Home.c
@@ -0,0 +1,77 @@
+/*****************************************************************************/
+/* AURHOR       : leeldevlopment                                             */
+/* DATE         : Jan 15 , 2017                                              */
+/*****************************************************************************/
+#include <stdio.h>
+#include <conio.h>
+int main ()
+{
+    char name[15],ch,ch2,ch3,ch4,ch5,ch6,ch7;
+    printf("Enter your name: ");
+    scanf("%s",&name);
+    printf("Enter your symbol1: ");
+    scanf(" %c",&ch);
+    printf("Enter your symbol2: ");
+    scanf(" %c",&ch2);
+    printf("Enter your symbol3: ");
+    scanf(" %c",&ch3);
+    printf("Enter your symbol4: ");
+    scanf(" %c",&ch4);
+    printf("Enter your symbol5: ");
+    scanf(" %c",&ch5);
+    printf("Enter your symbol6: ");
+    scanf(" %c",&ch6);
+    printf("Enter your symbol7: ");
+    scanf(" %c",&ch7);
+    printf("\n%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c\n",ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch);
+    printf("%c\t\t\t\t\t\t\t\t\t%c\n",ch,ch);
+	printf("%c\t\t\tMY CondoMinium \t\t\t\t\t%c\n");
+	printf("%c\t\t\t\t\t\t\t\t\t%c\n",ch,ch);
+    printf("%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c\n",ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch);
+    printf("%c\t\t\t\t\t\t\t\t\t%c\n",ch,ch);
+    printf("%c                                 %c%c%c                                   %c\n",ch,ch2,ch2,ch2,ch);
+    printf("%c                                %c%c%c%c%c                                  %c\n",ch,ch2,ch2,ch2,ch2,ch2,ch);
+    printf("%c            %c%c                 %c%c%c%c%c%c%c                 %c%c\t\t%c\n",ch,ch4,ch4,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch4,ch4,ch);
+    printf("%c           %c  %c               %c%c%c%c%c%c%c%c%c               %c  %c\t\t%c\n",ch,ch5,ch6,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch5,ch6,ch);
+    printf("%c          %c    %c            %c%c%c%c%c%c%c%c%c%c%c%c%c            %c    %c\t\t%c\n",ch,ch5,ch6,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch5,ch6,ch);
+    printf("%c          %c%c%c%c%c%c      %c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c      %c%c%c%c%c%c\t\t%c\n",ch,ch4,ch4,ch4,ch4,ch4,ch4,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch4,ch4,ch4,ch4,ch4,ch4,ch);
+    printf("%c            %c%c   %c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c   %c%c \t\t%c\n",ch,ch3,ch3,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch3,ch3,ch);
+    printf("%c            %c%c   %c                                 %c   %c%c \t\t%c\n",ch,ch3,ch3,ch2,ch2,ch3,ch3,ch);
+    printf("%c            %c%c   %c                                 %c   %c%c \t\t%c\n",ch,ch3,ch3,ch2,ch2,ch3,ch3,ch);
+    printf("%c\t  %c%c%c%c%c%c%c%c%c                                 %c%c%c%c%c%c%c%c%c\t\t%c\n",ch,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch);
+    printf("%c\t  %c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c\t\t%c\n",ch,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch7,ch7,ch7,ch7,ch7,ch7,ch7,ch7,ch7,ch7,ch7,ch7,ch7,ch7,ch7,ch7,ch7,ch7,ch7,ch7,ch7,ch7,ch7,ch7,ch7,ch7,ch7,ch7,ch7,ch7,ch7,ch7,ch7,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch);
+    printf("%c\t  %c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c\t\t%c\n",ch,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch);
+    printf("%c\t  %c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c\t\t%c\n",ch,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch);
+    printf("%c\t  %c%c%c    %c%c   %c%c   %c%c   %c%c   %c%c   %c%c   %c%c   %c%c    %c%c%c\t\t%c\n",ch,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch);
+    printf("%c\t  %c%c%c   %c%c%c   %c%c   %c%c   %c%c   %c%c   %c%c   %c%c   %c%c%c   %c%c%c\t\t%c\n",ch,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch);
+	printf("%c\t  %c%c%c  %c%c%c%c   %c%c   %c%c   %c%c   %c%c   %c%c   %c%c   %c%c%c%c  %c%c%c\t\t%c\n",ch,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch);
+    printf("%c\t  %c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c\t\t%c\n",ch,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch);
+    printf("%c\t  %c%c%c    %c%c   %c%c   %c%c   %c%c   %c%c   %c%c   %c%c   %c%c    %c%c%c\t\t%c\n",ch,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch);
+    printf("%c\t  %c%c%c   %c%c%c   %c%c   %c%c   %c%c   %c%c   %c%c   %c%c   %c%c%c   %c%c%c\t\t%c\n",ch,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch);
+	printf("%c\t  %c%c%c  %c%c%c%c   %c%c   %c%c   %c%c   %c%c   %c%c   %c%c   %c%c%c%c  %c%c%c\t\t%c\n",ch,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch);
+    printf("%c\t  %c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c\t\t%c\n",ch,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch);
+    printf("%c\t  %c%c%c    %c%c   %c%c   %c%c   %c%c   %c%c   %c%c   %c%c   %c%c    %c%c%c\t\t%c\n",ch,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch);
+    printf("%c\t  %c%c%c   %c%c%c   %c%c   %c%c   %c%c   %c%c   %c%c   %c%c   %c%c%c   %c%c%c\t\t%c\n",ch,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch);
+	printf("%c\t  %c%c%c  %c%c%c%c   %c%c   %c%c   %c%c   %c%c   %c%c   %c%c   %c%c%c%c  %c%c%c\t\t%c\n",ch,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch);
+    printf("%c\t  %c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c\t\t%c\n",ch,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch);
+    printf("%c\t  %c%c%c    %c%c   %c%c   %c%c   %c%c   %c%c   %c%c   %c%c   %c%c    %c%c%c\t\t%c\n",ch,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch);
+    printf("%c\t  %c%c%c   %c%c%c   %c%c   %c%c   %c%c   %c%c   %c%c   %c%c   %c%c%c   %c%c%c\t\t%c\n",ch,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch);
+	printf("%c\t  %c%c%c  %c%c%c%c   %c%c   %c%c   %c%c   %c%c   %c%c   %c%c   %c%c%c%c  %c%c%c\t\t%c\n",ch,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch);
+	printf("%c\t  %c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c\t\t%c\n",ch,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch);
+    printf("%c\t  %c%c%c    %c%c   %c%c   %c%c   %c%c   %c%c   %c%c   %c%c   %c%c    %c%c%c\t\t%c\n",ch,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch);
+    printf("%c\t  %c%c%c   %c%c%c   %c%c   %c%c   %c%c   %c%c   %c%c   %c%c   %c%c%c   %c%c%c\t\t%c\n",ch,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch);
+	printf("%c\t  %c%c%c  %c%c%c%c   %c%c   %c%c   %c%c   %c%c   %c%c   %c%c   %c%c%c%c  %c%c%c\t\t%c\n",ch,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch);
+	printf("%c\t  %c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c\t\t%c\n",ch,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch);
+	printf("%c\t  %c%c%c                                             %c%c%c\t\t%c\n",ch,ch2,ch2,ch2,ch2,ch2,ch2,ch);
+    printf("%c\t  %c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c\t\t%c\n",ch,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch);
+	printf("%c\t  %c%c%c                                             %c%c%c\t\t%c\n",ch,ch2,ch2,ch2,ch2,ch2,ch2,ch);
+    printf("%c\t  %c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c\t\t%c\n",ch,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch2,ch);
+    printf("%c\t\t\t\t\t\t\t\t\t%c\n",ch,ch);
+    printf("%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c\n",ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch);
+    printf("%c\t\t\t\t\t\t\t\t\t%c\n",ch,ch);
+	printf("%c\t\t\tDesign By %s\t\t\t%c\n",ch,name);
+	printf("%c\t\t\t\t\t\t\t\t\t%c\n",ch,ch);
+    printf("%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c",ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch);
+	getch();
+    return(0);
+    }
